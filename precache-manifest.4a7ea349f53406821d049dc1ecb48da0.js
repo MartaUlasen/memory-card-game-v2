7f511c02bb0f5b7b595297a9cd78ba32b45c369c
MartaUlasen <marta.ulasen@gmail.com>
2019-08-04 17:55:11 +0300

    Updates

---
+++ precache-manifest.4a7ea349f53406821d049dc1ecb48da0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a33424c011ced849c56402a7b1c25ffa",
+    "url": "/memory-card-game-v2/index.html"
+  },
+  {
+    "revision": "d1d14c4c675e4569b530",
+    "url": "/memory-card-game-v2/static/css/main.963e8249.chunk.css"
+  },
+  {
+    "revision": "705c2a191c9ada64fdb1",
+    "url": "/memory-card-game-v2/static/js/2.274c6bee.chunk.js"
+  },
+  {
+    "revision": "d1d14c4c675e4569b530",
+    "url": "/memory-card-game-v2/static/js/main.29882edf.chunk.js"
+  },
+  {
+    "revision": "d421880b54f78cc35b5d",
+    "url": "/memory-card-game-v2/static/js/runtime~main.acc74d1f.js"
+  }
+]);
